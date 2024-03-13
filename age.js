1937173a70fc52021f4fe62ba8ef7691ac39ab9f
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-03-14 00:05:03 +0200

    new changes

--- age.js
+++ age.js
@@ -1,1 +1,13 @@
 const date = document.getElementById('date');
+let btn = document.getElementById('btn')
+let dob = new Date();
+let display = document.querySelector('.display');
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const enteredDate = date.value;
+    let currentYear = dob.getFullYear();
+    let dateOfBirth = new Date(enteredDate);
+    let getYear = dateOfBirth.getFullYear();
+    let age = currentYear - getYear;
+    display.textContent = `Your age is ${age} years old`
+})
